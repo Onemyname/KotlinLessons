94e87dba8da11c2a4247a1b6dcdd323f06cff778
Onemyname <konvvadim@yandex.ru>
2023-02-12 19:18:33 +0400

    use scroll fiew

---
+++ ChangeLayouts/app/src/main/java/ru/konovalov/changelayouts/AboutActivity.kt
@@ -0,0 +1,13 @@
+package ru.konovalov.changelayouts
+
+import android.app.Activity
+import android.os.Bundle
+
+class AboutActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+
+
+    }
+}
---
+++ ChangeLayouts/app/src/main/java/ru/konovalov/changelayouts/SecondActivity.kt
@@ -0,0 +1,13 @@
+package ru.konovalov.changelayouts
+
+import android.app.Activity
+import android.os.Bundle
+
+class SecondActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.second_activity)
+
+
+    }
+}
