e98e55275e1d93bfe395a72a07d1c3b8bd063881
Onemyname <konvvadim@yandex.ru>
2023-05-12 13:31:19 +0300

    INIT: repo

---
+++ SQLTesting/src/main/java/ru/konovalov/App.java
@@ -0,0 +1,13 @@
+package ru.konovalov;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
